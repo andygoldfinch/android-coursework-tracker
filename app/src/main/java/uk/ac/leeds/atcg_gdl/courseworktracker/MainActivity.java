16acc53b1114e98114e86d1cdc0ccdaa5a28bacb
Andy Goldfinch <sc14atcg@leeds.ac.uk>
2015-12-07 10:53:46 +0000

    Create project

---
+++ app/src/main/java/uk/ac/leeds/atcg_gdl/courseworktracker/MainActivity.java
@@ -0,0 +1,13 @@
+package uk.ac.leeds.atcg_gdl.courseworktracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
