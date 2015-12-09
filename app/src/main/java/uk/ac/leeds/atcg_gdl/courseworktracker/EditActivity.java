23a3a3faa8c9025e555f386a410629e22d1dce59
Andy Goldfinch <sc14atcg@leeds.ac.uk>
2015-12-09 14:03:34 +0000

    Link main and edit activities

--- app/src/main/java/uk/ac/leeds/atcg_gdl/courseworktracker/EditActivity.java
+++ app/src/main/java/uk/ac/leeds/atcg_gdl/courseworktracker/EditActivity.java
@@ -1,9 +1,11 @@
 package uk.ac.leeds.atcg_gdl.courseworktracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
+
 import java.util.*;
 
 /**
@@ -11,18 +13,50 @@ import java.util.*;
  */
 public class EditActivity extends AppCompatActivity {
 
+    private EditText textModule;
+    private EditText textName;
+    private DatePicker datePicker;
+    private EditText weightText;
+    private EditText textNotes;
+    private CheckBox checkbox;
+
+    public EditActivity() {
+
+    }
+
     /**
      * Creates the activity.
+     *
      * @param savedInstanceState
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+        loadWidgets();
+
+        Intent startingIntent = getIntent();
+        if (startingIntent.hasExtra("coursework")) {
+            Coursework coursework = (Coursework) startingIntent.getSerializableExtra("coursework");
+            displayCoursework(coursework);
+        }
+    }
+
+    /**
+     * Get references to all of the widgets and assign them to the fields in the class.
+     */
+    private void loadWidgets() {
+        textModule = (EditText) findViewById(R.id.editTextModule);
+        textName = (EditText) findViewById(R.id.editTextName);
+        datePicker = (DatePicker) findViewById(R.id.datePickerDeadline);
+        weightText = (EditText) findViewById(R.id.editTextWeight);
+        textNotes = (EditText) findViewById(R.id.editTextNotes);
+        checkbox = (CheckBox) findViewById(R.id.checkbox);
     }
 
     /**
      * Attempts to save the coursework when the save button is pressed.
+     *
      * @param view
      */
     public void handleButtonSave(View view) {
@@ -41,27 +75,48 @@ public class EditActivity extends AppCompatActivity {
         new Database(getApplicationContext()).saveCoursework(coursework);
     }
 
+    private void displayCoursework(Coursework coursework)
+    {
+        setModuleName(coursework.getModuleName());
+        setCourseworkName(coursework.getCourseworkName());
+        setDeadline(coursework.getDeadline());
+        setWeight(coursework.getWeight());
+        setNotes(coursework.getNotes());
+        setCompleted(coursework.getCompleted());
+    }
+
     /**
      * @return The module name from the ui
      */
     private String getModuleName() {
-        EditText text = (EditText) findViewById(R.id.editTextModule);
-        return text.getText().toString();
+        return textModule.getText().toString();
+    }
+
+    /**
+     * @param text The module name
+     */
+    private void setModuleName(String text) {
+        textModule.setText(text);
     }
 
     /**
      * @return The coursework name from the ui
      */
     private String getCourseworkName() {
-        EditText text = (EditText) findViewById(R.id.editTextName);
-        return text.getText().toString();
+        return textName.getText().toString();
+    }
+
+    /**
+     * @param text The coursework name
+     */
+    private void setCourseworkName(String text) {
+        textName.setText(text);
     }
 
     /**
      * @return The deadline date from the ui
      */
     private Date getDeadline() {
-        DatePicker datePicker = (DatePicker) findViewById(R.id.datePickerDeadline);
         Calendar calendar = Calendar.getInstance();
         calendar.set(
                 datePicker.getDayOfMonth(),
@@ -73,25 +128,75 @@ public class EditActivity extends AppCompatActivity {
     }
 
     /**
+     * @param date The deadline of the coursework
+     */
+    private void setDeadline(Date date) {
+        Calendar calendar = new GregorianCalendar();
+        calendar.setTime(date);
+
+        datePicker.updateDate(
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
+        );
+    }
+
+    /**
      * Attempts to retrived the weight value from the ui.
      * Displays a toast to the user if validation fails.
      *
      * @return The weight value, or -1 if it is not valid.
      */
     private int getWeight() {
-        String weightText = ((EditText) findViewById(R.id.editTextWeight)).getText().toString();
-        if (weightText.length() == 0) {
+        String text = weightText.getText().toString();
+        if (text.length() == 0) {
             createToast("Weight cannot be blank");
             return -1;
         }
 
-        int weight = Integer.parseInt(weightText);
+        int weight = Integer.parseInt(text);
         if (weight < 0 || weight > 100) {
             createToast("Weight must be between 0 & 100");
             return -1;
         }
 
         return weight;
+    }
+
+    /**
+     * @param number The weight of the coursework
+     */
+    private void setWeight(int number) {
+        weightText.setText(String.valueOf(number));
+    }
+
+
+    /**
+     * @return The notes text from the ui
+     */
+    private String getNotes() {
+        return textNotes.getText().toString();
+    }
+
+    /**
+     * @param text The notes for the coursework
+     */
+    private void setNotes(String text) {
+        textNotes.setText(text);
+    }
+
+    /**
+     * @return Whether the coursework has been marked as completed.
+     */
+    private boolean getCompleted() {
+        return checkbox.isChecked();
+    }
+
+    /**
+     * @param complete The value for the completed checkbox
+     */
+    private void setCompleted(boolean complete) {
+        checkbox.setChecked(complete);
     }
 
     /**
@@ -103,20 +208,4 @@ public class EditActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
     }
 
-    /**
-     * @return The notes text from the ui
-     */
-    private String getNotes() {
-        EditText text = (EditText) findViewById(R.id.editTextNotes);
-        return text.getText().toString();
-    }
-
-    /**
-     * @return Whether the coursework has been marked as completed.
-     */
-    private boolean getCompleted()
-    {
-        CheckBox checkbox = (CheckBox)findViewById(R.id.checkbox);
-        return checkbox.isChecked();
-    }
 }
